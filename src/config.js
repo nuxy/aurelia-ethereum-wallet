46287fdef12571106ec7212cf54b174ea3fed1c5
Marc S. Brooks <marc@mbrooks.info>
2018-04-13 01:46:52 -0700

    Added application configuration file

---
+++ src/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Provides wallet configuration options.
+ */
+export class Config {
+
+  // Provider network (e.g. kovan homestead rinkeby ropsten).
+  static providerName = '';
+
+  // JSON-RPC URL the provider is connected to.
+  static jsonRpcProviderUrl = '';
+}
