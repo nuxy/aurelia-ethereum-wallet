36e9f45f2390efcb01710f484659b061dc648cb1
Marc S. Brooks <marc@mbrooks.info>
2018-04-15 13:47:01 -0700

    Added ability to create new wallet addresses

--- src/wallet/accounts.js
+++ src/wallet/accounts.js
@@ -1,12 +1,52 @@
+import ethers from 'ethers';
+
 /**
- * Wallet / Accounts.
+ * Wallet Accounts.
  */
 export class WalletAccounts {
+
+  /**
+   * @var {Array} accounts
+   */
+  accounts = [];
 
   /**
    * Create a new instance of WalletAccounts.
    */
   constructor() {
 
+  }
+
+  /**
+   * Create a new account.
+   */
+  create() {
+    let account = ethers.Wallet.createRandom();
+
+    this.accounts.push({
+      address: account.address,
+      balance: '0.00000000'
+    });
+  }
+
+  /**
+   * Rename an existing account.
+   */
+  rename(address, value) {
+    this.accounts = this.accounts.map(account => {
+      if (account.address === address) {
+        account.title = value;
+      }
+      return account;
+    });
+  }
+
+  /**
+   * Remove an existing account.
+   */
+  remove(address) {
+    this.accounts = this.accounts.filter(account => {
+      return account.address !== address;
+    });
   }
 }
