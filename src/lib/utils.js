5cd8416bdc475deee667081c2be654b4f4170dd7
Marc S. Brooks <marc@mbrooks.info>
2018-04-18 12:05:24 -0700

    Added Utils library for storing common functions
    Added promiseTasks method to execute sequential promised operations

---
+++ src/lib/utils.js
@@ -0,0 +1,51 @@
+/**
+ * Provides utility methods.
+ */
+export class Utils {
+
+  /**
+   * Create a new instance of Utils.
+   */
+  constructor() {
+
+  }
+
+  /**
+   * Execute promised operation in sequential order.
+   *
+   * @memberof Utils
+   * @method promiseTasks
+   *
+   * @param {String} name
+   *   Task name.
+   *
+   * @param {Array} tasks
+   *   Array of promised actions.
+   *
+   * @return {Promise}
+   *
+   * @example
+   *   let tasks = [
+   *     Promise.resolve(),
+   *     Promise.resolve(),
+   *     Promise.resolve(),
+   *
+   *     ..
+   *   ];
+   *
+   *   Utils.promiseTasks('Operation', tasks)
+   *     .then(obj => obj)
+   *     .catch(function(err) {
+   *
+   *     });
+   */
+  promiseTasks(name, tasks) {
+    return tasks.reduce(function(current, next) {
+      return current.then(next);
+    }, Promise.resolve([]))
+      .then(obj => obj)
+      .catch(function(err) {
+        throw new Error(`${name} ${err}`);
+      });
+  }
+}
