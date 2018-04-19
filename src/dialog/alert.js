4a9b19b9d14b1d2e570ca74b90c19732c31c0a50
Marc S. Brooks <marc@mbrooks.info>
2018-04-19 09:18:23 -0700

    Added alert dialog model, view, and SCSS

---
+++ src/dialog/alert.js
@@ -0,0 +1,37 @@
+import {inject}           from 'aurelia-dependency-injection';
+import {DialogController} from 'aurelia-dialog';
+
+@inject(DialogController)
+
+/**
+ * Dialog / Alert.
+ *
+ * @requires DialogController
+ */
+export class DialogAlert {
+
+  /**
+   * @var {String} message
+   */
+  message = null;
+
+  /**
+   * Create a new instance of DialogController.
+   *
+   * @param {DialogController} DialogController
+   *   DialogController instance.
+   */
+  constructor(DialogController) {
+    this.dialog = DialogController;
+
+    // Dialog properties.
+    this.dialog.settings.centerHorizontalOnly = true;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  activate(message) {
+    this.message = message;
+  }
+}
